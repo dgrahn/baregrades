146e448ef417224f8ede1b4ee99e4ddad8f88f62
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-09-08 19:43:37 +0000

    Add courses, users, and roles scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BareGrades::Application.routes.draw do
+  resources :roles
+
+  resources :users
+
+  resources :courses
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
