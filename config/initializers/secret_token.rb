ce2efc6608000909c112665d429253af673890a6
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-09-08 18:09:59 +0000

    This is the basic Ruby on Rails application structure. We are about to test this in the SDL (Senior Design Lab). Hopefully all the different computers will be able to run it fine. If this is the case, this will be the code we work from for the rest of the semester.
    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BareGrades::Application.config.secret_token = 'e50a8fbd70f109ba92b66a087e13aa0e9422ad080fbb0a9ccc27598bea4b839e2d3d4dfdc7b8824e18263c8f51ccf7293728e156221be3a7c6306f669c728c24'
