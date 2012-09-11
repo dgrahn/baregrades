dc3562ace8e982506e3a15f89f7de41a622a5929
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-09-11 00:20:31 +0000

    Add access table migration and connect classes, roles, and users.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ActiveRecord::Base
   attr_accessible :credits, :description, :identifier, :name, :pin, :points_based, :section, :student_managed
+  
+  has_many :access
+  has_many :users, :through => :access
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120908192651) do
+ActiveRecord::Schema.define(:version => 20120911000902) do
+
+  create_table "access", :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.integer "course_id"
+  end
 
   create_table "courses", :force => true do |t|
     t.string   "name"
