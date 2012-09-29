ccdcf1b427a2489b58c94435f0a62452a823b4fc
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-09-29 18:16:02 +0000

    Add a few styles for flashes. Start controlling access to the application except when logging in and registering.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	
+	before_filter :require_login
+	private
+	def require_login
+		unless current_user
+			redirect_to login_url
+		end
+	end
 
 	helper_method :current_user
-
 	private
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
