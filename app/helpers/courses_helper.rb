ef714f77aeaa9d16294e6ed6e525d9c5332723d8
jengelvormund <jengelvormund@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-10-06 21:20:36 +0000

    Added a course helper to get the number of students in a course.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,6 @@
 module CoursesHelper
+	def number_of_students
+		studentRoleId = Role.find_by_name("Student").id
+		Access.count(:conditions => {:course_id => @course.id, :role_id => studentRoleId});
+	end
 end
