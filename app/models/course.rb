a0a21db716674768fe84f42c750b1e66e7dba163
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-10-13 18:32:26 +0000

    Add some nicer formatting to grade_letter_class

--- app/models/course.rb
+++ app/models/course.rb
@@ -25,35 +25,35 @@ class Course < ActiveRecord::Base
 	def grade_letter_class(grade)		
 		if !grade_scale || !grade
 			return 'na'
-		elsif grade_scale.a_plus && grade_scale.a_plus <= grade
+		elsif grade_scale.a_plus 	&& grade_scale.a_plus 	<= grade
 			return 'Aplus'
-		elsif grade_scale.a && grade_scale.a <= grade
+		elsif grade_scale.a 		&& grade_scale.a 		<= grade
 			return 'A'
-		elsif grade_scale.a_minus && grade_scale.a_minus <= grade
+		elsif grade_scale.a_minus 	&& grade_scale.a_minus 	<= grade
 			return 'Aminus'
-		elsif grade_scale.b_plus && grade_scale.b_plus <= grade
+		elsif grade_scale.b_plus 	&& grade_scale.b_plus 	<= grade
 			return 'Bplus'
-		elsif grade_scale.b && grade_scale.b <= grade
+		elsif grade_scale.b 		&& grade_scale.b 		<= grade
 			return 'B'
-		elsif grade_scale.b_minus && grade_scale.b_minus <= grade
+		elsif grade_scale.b_minus 	&& grade_scale.b_minus 	<= grade
 			return 'Bminus'
-		elsif grade_scale.c_plus && grade_scale.c_plus <= grade
+		elsif grade_scale.c_plus 	&& grade_scale.c_plus 	<= grade
 			return 'Cplus'
-		elsif grade_scale.c && grade_scale.c <= grade
+		elsif grade_scale.c 		&& grade_scale.c 		<= grade
 			return 'C'
-		elsif grade_scale.c_minus && grade_scale.c_minus <= grade
+		elsif grade_scale.c_minus 	&& grade_scale.c_minus 	<= grade
 			return 'Cminus'
-		elsif grade_scale.d_plus && grade_scale.d_plus <= grade
+		elsif grade_scale.d_plus 	&& grade_scale.d_plus 	<= grade
 			return 'Dplus'
-		elsif grade_scale.d && grade_scale.d <= grade
+		elsif grade_scale.d 		&& grade_scale.d 		<= grade
 			return 'D'
-		elsif grade_scale.d_minus && grade_scale.d_minus <= grade
+		elsif grade_scale.d_minus 	&& grade_scale.d_minus 	<= grade
 			return 'Dminus'
-		elsif grade_scale.f_plus && grade_scale.f_plus <= grade
+		elsif grade_scale.f_plus 	&& grade_scale.f_plus 	<= grade
 			return 'Fplus'
-		elsif grade_scale.f && grade_scale.f <= grade
+		elsif grade_scale.f 		&& grade_scale.f 		<= grade
 			return 'F'
-		elsif grade_scale.f_minus && grade_scale.f_minus <= grade
+		elsif grade_scale.f_minus 	&& grade_scale.f_minus <= grade
 			return 'Fminus'
 		else
 			return 'na'
