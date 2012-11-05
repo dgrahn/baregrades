88cf30e4eaa17339a38d185a675fe7e94d9ab8d4
jengelvormund <jengelvormund@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-11-05 19:06:01 +0000

    Send bug reports to admin@baregrades.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.feedback.subject
-  #
-  def feedback(email, topic, description)
-	@email = email
-    @topic = topic
-	@description = description
-
-    mail(:to => "jtengel08@gmail.com", :from => @email, :subject => @topic, :body => @description)
-  end
+	default :to => "admin@baregrades.com"
+	
+	def feedback(email, topic, description)
+	
+		mail(:to 		=> "admin@baregrades.com",
+			 :from 		=> email,
+			 :subject 	=> topic,
+			 :body 		=> description)
+	end
 end
