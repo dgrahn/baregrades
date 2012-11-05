f2736f746f6057963aee04fa01534812e42dee03
jengelvormund <jengelvormund@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-11-05 19:14:36 +0000

    Added cc for feedback email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ActionMailer::Base
 	default :to => "admin@baregrades.com"
+			
 	
 	def feedback(email, topic, description)
 	
 		mail(:to 		=> "admin@baregrades.com",
+			 :cc 		=> ["jengel@cedarville.edu", "dgrahn@cedarville.edu", "mbrooker@cedarville.edu"],
 			 :from 		=> email,
 			 :subject 	=> topic,
 			 :body 		=> description)
