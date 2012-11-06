93a98a81c377088031fea7578188a88eb3a28ba3
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-11-06 14:47:53 +0000

    Add class average to assignment. Remove flash from login. Comment the assignment model.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,13 @@
+# The program creates a way for students to manage and post
+# their grades from various classes. Students can collaborate
+# on classes and view course averages.
+#
+# Author:: Dan Grahn, Matt Brooker, Justin Engel
+#
+# This class is the model for assignments. It accesses and 
+# manipulates the data to give grades, percentages, and
+# averages.
+
 class Assignment < ActiveRecord::Base
 	attr_accessible :description, :name, :worth, :assignment_type_id, :due_date
 
@@ -10,6 +20,7 @@ class Assignment < ActiveRecord::Base
 	has_many :grades
 	
 
+	# Get the current users grade for the assignment (points)
 	def user_grade(user)
 		grade = Grade.where(:assignment_id => self.id, :user_id => user.id).first
 		
@@ -18,6 +29,7 @@ class Assignment < ActiveRecord::Base
 		end
 	end
 	
+	# Get the current users percentage for the assignment.
 	def user_percentage(user)
 		worth 		= self.worth
 		grade 		= self.user_grade(user)
@@ -28,7 +40,20 @@ class Assignment < ActiveRecord::Base
 		end
 	end
 	
+	# Get the course for this assignment. This is necessary
+	# because belongs to cannot have a through.
 	def course
 		return self.assignment_type.course
+	end
+
+	# Get the average grade for this assignment. This will only
+	# return the average if there are more than 2 students in
+	# the course (for privacy reasons).
+	def average
+		if course.users.length > 2
+			average = (grades.sum(:grade) / grades.length);
+			average = (average / worth) * 100;
+			return average
+		end
 	end
 end
