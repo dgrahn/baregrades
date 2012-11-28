9d75265c294838b2f6dc1cadf5ef0a7e0e9cfcfd
matt-bro <matt-bro@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-11-28 19:46:36 +0000

    Moved manage_grade_path to the application_helper.rb so it could be used in the application_type view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,4 +38,12 @@ module ApplicationHelper
 			}
 		}
 	end
+	
+	def manage_grade_path(assignment, clas)
+		if assignment.user_grade(@current_user).blank?
+			link_to "Add Grade", new_assignment_grade_path(assignment), :class => clas
+		else
+			link_to "Edit Grade", edit_assignment_grade_path(assignment), :class => clas 
+		end
+	end
 end
--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,9 +1,3 @@
 module AssignmentsHelper
-	def manage_grade_path(assignment, clas)
-		if assignment.user_grade(@current_user).blank?
-			link_to "Add Grade", new_assignment_grade_path(assignment), :class => clas
-		else
-			link_to "Edit Grade", edit_assignment_grade_path(assignment), :class => clas 
-		end
-	end
+
 end
