c02a53e96cdaa1974d4e8b904b1f0c886e8a11c8
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2012-12-10 03:52:51 +0000

    Added darkness theme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -106,6 +106,8 @@ module ApplicationHelper
 				return "http://fonts.googleapis.com/css?family=Lato:300,400,700,900"
 			when "citrus"
 				return "http://fonts.googleapis.com/css?family=Permanent+Marker"
+			when "darkness"
+				return "http://fonts.googleapis.com/css?family=Old+Standard+TT"
 		end
 	end
 end
