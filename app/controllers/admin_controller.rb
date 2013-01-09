726bd328e665b56ceb12a45d7368a0ed50973a2b
dgrahn <dgrahn@070df8e3-dea0-439a-8e91-66bc3ec99183>
2013-01-09 21:09:44 +0000

    Add a few cool admin statistics.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,16 @@
 class AdminController < ApplicationController
 	def index
+		@number_of_users = User.count
+		@number_of_courses = Course.count
+		@number_of_assignments = Assignment.count
+		@number_of_grades = Grade.count
+		
+		@users_per_course = 0
+
+		Course.all.each do |course|
+			@users_per_course += course.users.length
+		end
+		
+		@users_per_course = @users_per_course.to_f / @number_of_courses.to_f
 	end
 end
