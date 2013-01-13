f2d8815d09f25b398accceb52d6a77580908fb6f
jengelvormund <jengelvormund@070df8e3-dea0-439a-8e91-66bc3ec99183>
2013-01-13 20:02:15 +0000

    Removed things that were not being used

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,6 +1,2 @@
 module CoursesHelper
-	def number_of_students
-		studentRoleId = Role.find_by_name("Student").id
-		Access.count(:conditions => {:course_id => @course.id, :role_id => studentRoleId});
-	end
 end
