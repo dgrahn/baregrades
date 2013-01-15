8dadb7ec4e945a092c50e075432ad82f1c90fa86
jengelvormund <jengelvormund@070df8e3-dea0-439a-8e91-66bc3ec99183>
2013-01-15 23:17:24 +0000

    added an email_confirm field to ensure users enter the correct email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 # -----------------------------------------------------------
 
 class User < ActiveRecord::Base
-	attr_accessible :email, :first_name, :last_name, :middle_name, :username, :password, :password_confirmation, :theme_id
+	attr_accessible :email, :email_confirmation, :first_name, :last_name, :middle_name, :username, :password, :password_confirmation, :theme_id
 	attr_accessor :password
 
 	before_save :encrypt_password
@@ -22,8 +22,8 @@ class User < ActiveRecord::Base
 	belongs_to :theme
 
 	validates :username,	:presence => true, :uniqueness => true
-	validates :password,	:presence => true, :length => {:minimum => 5}, :confirmation => true, :on => "create"
-	validates :email, 		:presence => true, :uniqueness => true
+	validates :password,	:presence => true, :length => {:minimum => 5}, :confirmation => true
+	validates :email, 		:presence => true, :uniqueness => true, :confirmation => true
 	validates :first_name,	:presence => true
 	validates :last_name,	:presence => true
 	
